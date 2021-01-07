fa14f9fabcd4c1f0723a982835ac6d2d43195583
Pritom <pritomcsedu.56@gmail.com>
2021-01-07 09:08:13 +0600

    --- Auto-Git Commit ---

--- static/js/indexScripts.js
+++ static/js/indexScripts.js
@@ -1,1 +1,295 @@
-var myNode,user=localStorage.getItem("user"),pass=localStorage.getItem("pass"),fileTag=document.getElementById("filetag"),fileTag2=document.getElementById("filetag2"),preview=document.getElementById("preview"),token=localStorage.getItem("main-token"),decoded=jwt_decode(token);if(localStorage.setItem("token",token),localStorage.setItem("userID",decoded.id),localStorage.setItem("userEmail",decoded.email),localStorage.setItem("user",decoded.role[0].roleName),user=decoded.role[0].roleName,localStorage.removeItem("pass"),"SUPER_ADMIN"==user){for($("#logooo").hide(),document.title="Super Admin",myNode=document.getElementById("merchant");myNode.firstChild;)myNode.removeChild(myNode.firstChild);for(myNode.remove(),myNode=document.getElementById("organizationHead");myNode.firstChild;)myNode.removeChild(myNode.firstChild);myNode.remove(),(preview=document.getElementById("preview")).setAttribute("src","static/img/Logo.png"),document.getElementById("dashName").innerHTML="Super Admin's Panel"}else if("ORGANIZATIONAL_ADMIN"==user){function changeImage(e){var t;e.files&&e.files[0]&&((t=new FileReader).onload=function(e){$.ajax({type:"POST",url:urlForAll+"approved/insert/logo",data:JSON.stringify({user_id:localStorage.getItem("userID"),image_str:e.target.result}),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(t){$("#one1").hide(),$("#two2").show(),preview.setAttribute("src",e.target.result)}})},t.readAsDataURL(e.files[0]))}function changeImage2(e){var t;e.files&&e.files[0]&&((t=new FileReader).onload=function(e){$.ajax({type:"PUT",url:urlForAll+"approved/update/logo",data:JSON.stringify({user_id:localStorage.getItem("userID"),image_str:e.target.result}),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(t){$("#one1").hide(),$("#two2").show(),preview.setAttribute("src",e.target.result)}})},t.readAsDataURL(e.files[0]))}for($.ajax({type:"GET",url:urlForAll+"orgHead/get/logo/"+localStorage.getItem("userID"),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(e){""==e.data.image_str?$("#two2").hide():""!=e.data.image_str&&(preview.setAttribute("src",e.data.image_str),$("#one1").hide())}}),fileTag.addEventListener("change",(function(){changeImage(this)})),fileTag2.addEventListener("change",(function(){changeImage2(this)})),$.ajax({type:"GET",url:urlForAll+"orgHead/get/org/name/"+localStorage.getItem("userID"),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(e){document.getElementById("namee").innerHTML=e.data,document.getElementById("dashName").innerHTML=e.data+"'s Panel",document.title=e.data}}),myNode=document.getElementById("merchant");myNode.firstChild;)myNode.removeChild(myNode.firstChild);for(myNode.remove(),myNode=document.getElementById("superAdmin");myNode.firstChild;)myNode.removeChild(myNode.firstChild);myNode.remove()}else if("MERCHANT"==user){$("#logooo").hide(),$.ajax({type:"GET",url:urlForAll+"merchant/get/org/logo/"+localStorage.getItem("userID"),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(e){preview.setAttribute("src",e.data.image_str)}}),$.ajax({type:"GET",url:urlForAll+"merchant/get/org/name/"+localStorage.getItem("userID"),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(e){document.getElementById("namee").innerHTML=e.data}}),$.ajax({type:"GET",url:urlForAll+"profile/get/profile/"+localStorage.getItem("userID"),headers:{Accept:"application/json","Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("token")},success:function(e){document.title=e.data.sender_name,document.getElementById("dashName").innerHTML=e.data.sender_name+"'s Panel"}});fileTag=document.getElementById("filetag"),fileTag2=document.getElementById("filetag2");for(preview=document.getElementById("preview"),myNode=document.getElementById("organizationHead");myNode.firstChild;)myNode.removeChild(myNode.firstChild);for(myNode.remove(),myNode=document.getElementById("superAdmin");myNode.firstChild;)myNode.removeChild(myNode.firstChild);myNode.remove()}else localStorage.removeItem("user"),window.location.href="index.html";
+var myNode;
+var user = localStorage.getItem('user');
+var pass = localStorage.getItem('pass');
+var fileTag = document.getElementById("filetag");
+var fileTag2 = document.getElementById("filetag2");
+var preview = document.getElementById("preview");
+
+var token = localStorage.getItem('main-token');
+var decoded = jwt_decode(token);
+localStorage.setItem('token', token);
+localStorage.setItem('userID', decoded.id);
+localStorage.setItem('userEmail', decoded.email);
+localStorage.setItem('user', decoded.role[0].roleName);
+user = decoded.role[0].roleName;
+localStorage.removeItem('pass');
+
+if (user == 'SUPER_ADMIN') {
+	$('#logooo').hide();
+	document.title = "Super Admin";
+	myNode = document.getElementById("merchant");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();
+	myNode = document.getElementById("organizationHead");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();
+	/*myNode = document.getElementById("manager");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();*/
+
+	preview = document.getElementById("preview");
+	preview.setAttribute('src', 'static/img/Logo.png');
+	document.getElementById("dashName").innerHTML = "Super Admin's Panel";
+}
+else if (user == 'ORGANIZATIONAL_ADMIN') {
+	$.ajax
+		({
+			type: "GET",
+			url: urlForAll + "orgHead/get/logo/" + localStorage.getItem('userID'),
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			success: function (data) {
+				if (data.data.image_str == "") {
+
+					$('#two2').hide();
+				}
+				else if (data.data.image_str != "") {
+
+					preview.setAttribute('src', data.data.image_str);
+					$('#one1').hide();
+				}
+			}
+		});
+
+
+	fileTag.addEventListener("change", function () {
+
+		changeImage(this);
+	});
+	function changeImage(input) {
+		var reader;
+
+		if (input.files && input.files[0]) {
+			reader = new FileReader();
+
+			reader.onload = function (e) {
+				$.ajax
+					({
+						type: "POST",
+						url: urlForAll + "approved/insert/logo",
+						data: JSON.stringify
+							({
+								"user_id": localStorage.getItem('userID'),
+								"image_str": e.target.result
+							}),
+						headers:
+						{
+							'Accept': 'application/json',
+							'Content-Type': 'application/json',
+							"Authorization": 'Bearer ' + localStorage.getItem('token')
+						},
+						success: function (data) {
+							$('#one1').hide();
+							$('#two2').show();
+							preview.setAttribute('src', e.target.result);
+						}
+					});
+				//console.log(e.target.result);
+				//preview.setAttribute('src', e.target.result);
+			}
+
+			reader.readAsDataURL(input.files[0]);
+		}
+	}
+
+	fileTag2.addEventListener("change", function () {
+		changeImage2(this);
+	});
+
+	function changeImage2(input2) {
+		var reader;
+
+		if (input2.files && input2.files[0]) {
+			reader = new FileReader();
+
+			reader.onload = function (e) {
+
+				$.ajax
+					({
+
+						type: "PUT",
+						url: urlForAll + "approved/update/logo",
+						data: JSON.stringify
+							({
+								"user_id": localStorage.getItem('userID'),
+								"image_str": e.target.result
+							}),
+						headers:
+						{
+							'Accept': 'application/json',
+							'Content-Type': 'application/json',
+							"Authorization": 'Bearer ' + localStorage.getItem('token')
+						},
+						success: function (data) {
+							$('#one1').hide();
+							$('#two2').show();
+							preview.setAttribute('src', e.target.result);
+						}
+					});
+				//console.log(e.target.result);
+			}
+
+			reader.readAsDataURL(input2.files[0]);
+		}
+	}
+
+	$.ajax
+		({
+
+			type: "GET",
+			url: urlForAll + "orgHead/get/org/name/" + localStorage.getItem('userID'),
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			success: function (data) {
+				document.getElementById("namee").innerHTML = data.data;
+				document.getElementById("dashName").innerHTML = data.data + "'s Panel";
+				document.title = data.data;
+			}
+		});
+	myNode = document.getElementById("merchant");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();
+	myNode = document.getElementById("superAdmin");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();
+	/*myNode = document.getElementById("manager");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();*/
+	//document.getElementById("dashName").innerHTML = "Organization Head's Panel";
+}
+/*else if(user == 'MANAGER')
+{
+	$('#logooo').hide();
+	document.title = "Manager";
+	$.ajax
+	({
+		
+		type: "GET",
+		url: urlForAll + "manager/profile/" + localStorage.getItem('userID'),
+		headers: 
+		{
+		  'Accept': 'application/json',
+		  'Content-Type': 'application/json',
+		  "Authorization": 'Bearer ' + localStorage.getItem('token')
+		},
+		success: function(data) 
+		{
+			localStorage.setItem('managerEmail', data.data.manager_email);
+			myNode = document.getElementById("merchant");
+			while (myNode.firstChild) {
+				myNode.removeChild(myNode.firstChild);
+			}
+			myNode.remove();
+			myNode = document.getElementById("organizationHead");
+			while (myNode.firstChild) {
+				myNode.removeChild(myNode.firstChild);
+			}
+			myNode.remove();
+			myNode = document.getElementById("superAdmin");
+			while (myNode.firstChild) {
+				myNode.removeChild(myNode.firstChild);
+			}
+			myNode.remove();
+			localStorage.setItem("ccd", data.data.can_create_delivery);
+			localStorage.setItem("ccdm", data.data.can_create_delivery_man);
+			var canCreateDelivery = data.can_create_delivery;
+			var canCreateDeliveryMan = data.can_create_delivery_man;
+			document.getElementById("dashName").innerHTML = "Manager's Panel";				
+		}
+	});
+}*/
+else if (user == 'MERCHANT') {
+	$('#logooo').hide();
+
+	$.ajax
+		({
+
+			type: "GET",
+			url: urlForAll + "merchant/get/org/logo/" + localStorage.getItem('userID'),
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			success: function (data) {
+				preview.setAttribute('src', data.data.image_str);
+			}
+		});
+	$.ajax
+		({
+
+			type: "GET",
+			url: urlForAll + "merchant/get/org/name/" + localStorage.getItem('userID'),
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			success: function (data) {
+				document.getElementById("namee").innerHTML = data.data;
+			}
+		});
+
+	$.ajax
+		({
+
+			type: "GET",
+			url: urlForAll + "profile/get/profile/" + localStorage.getItem('userID'),
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			success: function (data) {
+				document.title = data.data.sender_name;
+				document.getElementById("dashName").innerHTML = data.data.sender_name + "'s Panel";
+			}
+		})
+
+	var fileTag = document.getElementById("filetag");
+	var fileTag2 = document.getElementById("filetag2");
+	preview = document.getElementById("preview");
+	//document.getElementById("dashName").innerHTML = "Merchant's Panel";
+	myNode = document.getElementById("organizationHead");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();
+	myNode = document.getElementById("superAdmin");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();
+	/*myNode = document.getElementById("manager");
+	while (myNode.firstChild) {
+		myNode.removeChild(myNode.firstChild);
+	}
+	myNode.remove();*/
+}
+else {
+	localStorage.removeItem('user');
+	window.location.href = "index.html";
+}
