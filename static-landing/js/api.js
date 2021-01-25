6d4f8fd31fb129d48276c81dd52d63e09effb1f5
Pritom <pritomcsedu.56@gmail.com>
2021-01-25 15:39:46 +0600

    --- Auto-Git Commit ---

--- static-landing/js/api.js
+++ static-landing/js/api.js
@@ -1,16 +1,1 @@
 const api = "https://api-new.quixx.xyz/api/quixx/v1/";
-
-function Hell() {
-    this.hello = "ds";
-
-    // this.b = () => {
-    //     console.log(this.hello);
-    // }
-    // this.f = function b() {
-    //     console.log(this.hello);
-    // }
-}
-
-const aa = new Hell();
-
-aa.f();
