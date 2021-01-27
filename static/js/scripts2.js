3aea8ddb17fe02293b880a504bc6e038ff6ce66a
Pritom <pritomcsedu.56@gmail.com>
2021-01-28 05:26:50 +0600

    --- Auto-Git Commit ---

--- static/js/scripts2.js
+++ static/js/scripts2.js
@@ -1,7 +1,5 @@
 jQuery(function ($) {
-    'use strict';
     $(function () {
-        $('#status').delay(5200).fadeOut('slow');
-        $('#preloader').delay(5200).fadeOut('slow');
+        // $('.preloader').delay(4000).fadeOut('slow');
     });
 });
