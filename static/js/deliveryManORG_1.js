b5840e998650cd99922261af066900accf5e7afb
Pritom <pritomcsedu.56@gmail.com>
2021-02-08 06:17:12 +0600

    --- Auto-Git Commit ---

--- static/js/deliveryManORG_1.js
+++ static/js/deliveryManORG_1.js
@@ -40,4 +40,43 @@ $(document).ready(function () {
 				}
 			}
 		});
+
+	var district = ["Barguna", "Barishal", "Bhola", "Cumilla", "Dinajpur"];
+	var districts1 = ["ab", "b", "c"];
+	var districts2 = ["ab2", "b2", "c2"];
+	var districts3 = ["ab", "b", "c"];
+	var dhakaIndex;
+	$('#district')
+		.empty();
+	for (var i = 0; i < district.length; i++) {
+		if (district[i] === "Barishal") {
+			dhakaIndex = i;
+		}
+		var optionTest = new Option(district[i], district[i]);
+		$(optionTest).html(district[i]);
+		$("#district").append(optionTest);
+	}
+	document.getElementById('district').selectedIndex = dhakaIndex; //area = "Dhaka";
+
+	// for (var i = 0; i < districts1.length; i++) {
+	// 	var option = new Option(districts1[i], districts1[i]);
+	// 	$(option).html(districts1[i]);
+	// 	$("#managers_2").append(option);
+	// }
+	document.querySelector("#district").addEventListener("change", function () {
+		var vari = this.value == "Barguna" ? districts1 : this.value == "Barishal" ? districts2 : null;
+
+		let deliverArea = document.querySelector(".deliveryArea");
+		deliverArea.innerHTML = "";
+		let checkbox = "", textOfCheckBox = "";
+		for (let i of vari) {
+			checkbox = document.createElement("input");
+			checkbox.type = "checkbox";
+			checkbox.value = i;
+			checkbox.name = "mycheckboxes";
+
+			textOfCheckBox = document.createTextNode(i);
+			deliverArea.append(checkbox, textOfCheckBox);
+		}
+	});
 });
