4f93ceab5752e0a9bb0f1313b95bd3ce5be4d9cb
Pritom <pritomcsedu.56@gmail.com>
2021-03-13 10:21:43 +0600

    --- Auto-Git Commit ---

---
+++ static/js/setSMS.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#settings").hide();
+});
+
+let Org_JC = document.querySelector("Org_JC");
+let Mer_JC = document.querySelector("Mer_JC");
+let Receiver_JC = document.querySelector("Receiver_JC");
