ab95280596845e3248ffc2f0ea0a119f0dbcb5b0
Pritom <pritomcsedu.56@gmail.com>
2021-04-19 08:22:14 +0600

    --- Auto-Git Commit ---

--- static/js/customCriteria_1.js
+++ static/js/customCriteria_1.js
@@ -1,19 +1,17 @@
 var org_ID = localStorage.getItem('userID');
 var org_Email = localStorage.getItem('userEmail');
-var day = weight = distance = type = 0;
+var day = weight = distance = type = flag = 0;
 $(document).ready(function () {
     $('#settings').hide();
     $('.container').hide();
     $('#createCriteria').hide();
 
     function createFieldStart(data) {
-        console.log(Object.keys(data), data);
         let div;
         Object.keys(data).map(item => {
             div = document.querySelector(`.col.${item}`);
             for (let i of data[item]) {
                 createField(item, i);
-                // item === "dayType" ? day++ : item === "productType" ? type++ : item === "weight" ? weight++ : item === "distance" ? distance++ : null;
             }
         });
     }
@@ -42,7 +40,10 @@ $(document).ready(function () {
             "Authorization": 'Bearer ' + localStorage.getItem('token')
         },
         success: function (data) {
-            data.data === true ? fillInput() : null;
+            if (data.data === true) {
+                fillInput();
+                flag = 1;
+            }
         }
     });
 });
