d0428b7ff35c0b10fd51e2c1d851124ca40d61ee
Pritom <pritomcsedu.56@gmail.com>
2021-04-20 01:26:38 +0600

    --- Auto-Git Commit ---

--- static/js/customCriteria_1.js
+++ static/js/customCriteria_1.js
@@ -1,40 +1,36 @@
 var org_ID = localStorage.getItem('userID');
 var org_Email = localStorage.getItem('userEmail');
 var day = weight = distance = type = flag = 0;
+function createFieldStart(data, isTab) {
+    let div;
+    Object.keys(data).map(item => {
+        div = document.querySelector(`.col.${item}`);
+        for (let i of data[item]) {
+            isTab ? fillInputDetails(item, i) : createField(item, i);
+        }
+    });
+}
+function fillInput(isTab = false) {
+    $.ajax({
+        url: urlForAll + "delivery/criteria/keys/" + org_ID,
+        type: "GET",
+        headers:
+        {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            "Authorization": 'Bearer ' + localStorage.getItem('token')
+        },
+        success: function (data) {
+            createFieldStart(data.data, isTab);
+
+        }
+    });
+}
 $(document).ready(function () {
     $('#settings').hide();
     $('.container').hide();
     $('#createCriteria').hide();
     document.getElementById('setCriteriaDetails').style.display = "none";
-    function createFieldStart(data) {
-        let div;
-        Object.keys(data).map(item => {
-            div = document.querySelector(`.col.${item}`);
-            for (let i of data[item]) {
-                createField(item, i);
-                fillInputDetails(item, i);
-            }
-        });
-    }
-    function fillInput() {
-        try {
-            $.ajax({
-                url: urlForAll + "delivery/criteria/keys/" + org_ID,
-                type: "GET",
-                headers:
-                {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    "Authorization": 'Bearer ' + localStorage.getItem('token')
-                },
-                success: function (data) {
-                    createFieldStart(data.data);
-                }
-            });
-        } catch (e) {
-            console.log(e + "Hello");
-        }
-    }
     $.ajax({
         url: urlForAll + "delivery/criteria/enable/" + org_ID,
         type: "GET",
