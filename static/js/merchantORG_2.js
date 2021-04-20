4d7924fc6ebc2f4dd46fda283a1a0bdf5426bc55
Pritom <pritomcsedu.56@gmail.com>
2021-04-20 09:29:01 +0600

    --- Auto-Git Commit ---

--- static/js/merchantORG_2.js
+++ static/js/merchantORG_2.js
@@ -268,7 +268,7 @@ function merchantActivateFunction(dataGet, table) {
 				table.rows().every(function (index, element) {
 					var row = $(this.node());
 					row.find('td').eq(8)[0].children[0].disabled = dataOTP.data.indexOf(dataGet.data[index].approved_merchant_id) !== -1 ? true : false;
-					row.find('td').eq(9)[0].children[0].disabled = !row.find('td').eq(7)[0].children[0].disabled;
+					row.find('td').eq(9)[0].children[0].disabled = !row.find('td').eq(8)[0].children[0].disabled;
 
 					// console.log(element, row, statusElement, row.find('td'));
 					// var isChecked = statusElement.prop('checked');
@@ -1252,4 +1252,153 @@ $('#dtBasicExampleActivate').on('click', '.btn-EditCriteria', function () {
 	$("#sure3Criteria").hide();
 	//$("#sure3").html("Are you sure?");
 	$("#myModalMerUpdateCriteria").modal('show');
+});
+
+
+var fillInputDetails = (types, values = undefined) => {
+	let typeForCreate = types === "dayType" ? ".dyn.dayType" : types === "productType" ? ".dyn.productType" : types === "weight" ? ".dyn.productWeight" : types === "distance" ? ".dyn.productDistance" : null;
+	let placeHolder = types === "dayType" ? "... Urgent, SameDay, NextDay ..." : types === "productType" ? "... Glass, Food ..." : types === "weight" ? "... 1-2, 1-4, 3, 0.6 ..." : types === "distance" ? "... 1-2, 2, 3, 0.4 ..." : null;
+	let classesName = types === "dayType" ? "qdc" : types === "productType" ? "qtc" : types === "weight" ? "qwc" : types === "distance" ? "qdsc" : null;
+	let increment = types === "dayType" ? day : types === "productType" ? type : types === "weight" ? weight : types === "distance" ? distance : null;
+
+	let div = document.querySelector(`${typeForCreate}`);
+
+	let dummyDivFlex = document.createElement("div");
+	dummyDivFlex.className = `${classesName}${increment} flexIt2`;
+
+	let identifier = document.createElement("span");
+	identifier.innerHTML = values !== undefined ? `${values}:` : "";
+	identifier.style.cssText = "color: #0066b3; margin:1rem 0; width: 12%;";
+
+	let input = document.createElement("input");
+	input.type = "text";
+	// identidier.value = values !== undefined ? values : "";
+	input.style.cssText = "color: #0066b3;";
+	input.className = `form-control ${typeForCreate.substr(5)}${values.replace(/ /g, "")}`;
+
+	types === "dayType" ? day++ : types === "productType" ? type++ : types === "weight" ? weight++ : types === "distance" ? distance++ : null;
+	dummyDivFlex.append(identifier, input);
+
+	div.append(dummyDivFlex);
+}
+
+async function setUpdateCriteria() {
+	document.getElementById('one-cc').disabled = false;
+	document.getElementById('two-cc').disabled = true;
+	document.getElementById('two-cc').style.fontSize = '14.5px';
+	document.getElementById('one-cc').style.fontSize = '13px';
+	document.getElementById('setCriteriaDetails').style.display = "block";
+	document.getElementById('createCriteria').style.display = "none";
+	if (flag) {
+		Array.from(document.querySelectorAll("#setCriteriaDetails .flexIt2")).map(item => item.remove());
+		await fillInput(true);
+		criteriaEnabled ? getData() : null;
+	}
+}
+
+function getData() {
+	$.ajax
+		({
+			type: "GET",
+			url: urlForAll + "delivery/criteria/" + org_ID,
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			success: function (data) {
+				Object.keys(data.data).map(item => {
+					if (item === "id" || item === "userId") {
+						return;
+					}
+					Array.from(Object.keys(data.data[item]).map(itemKeys => {
+						console.log(itemKeys);
+						console.log("");
+						console.log(document.querySelector(`.${item}${itemKeys.replace(/ /g, "")}`), `.${item}${itemKeys}`, `.${item}${itemKeys.replace(/ /g, "")}`);
+
+						document.querySelector(`.${item}${itemKeys.replace(/ /g, "")}`) ? document.querySelector(`.${item}${itemKeys.replace(/ /g, "")}`).value = data.data[item][itemKeys] : null;
+					}));
+				});
+			},
+			error: function (data) {
+
+			}
+		});
+}
+
+function fillupFields() {
+	if (flag) {
+		$('#tickActivateConfirm').hide();
+		$(".circle-loader").removeClass("load-complete");
+
+		$("#sureActivateConfirm").html("Are you sure?");
+		$("#myModalCriteriaConfirm").modal('show');
+	}
+}
+
+
+document.querySelector("#modalCriteriaSetConfirm").addEventListener("click", function () {
+	document.getElementById('modalCriteriaCancelConfirm').disabled = true;
+	document.getElementById('modalCriteriaSetConfirm').disabled = true;
+	let array = [];
+	Object.keys(fillData).map(item => {
+		let obj = {};
+		fillData[item].map(i => {
+			obj[i] = document.querySelector(`input[class*='${i.replace(/ /g, "")}']`).value ? document.querySelector(`input[class*='${i.replace(/ /g, "")}']`).value : 0;
+			// console.log(i, document.querySelector(`input[class*='${i.replace(/ /g, "")}']`));
+		});
+		array.push(obj);
+	});
+	console.log(array);
+	$.ajax
+		({
+			type: criteriaEnabled ? "PUT" : "POST",
+			url: `${urlForAll}delivery/criteria/${criteriaEnabled ? "update" : "create"}/${org_ID}`,
+			headers:
+			{
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization": 'Bearer ' + localStorage.getItem('token')
+			},
+			data: JSON.stringify({
+				"dayTypeMap": array[0],
+				"productTypeMap": array[1],
+				"productDistanceMap": array[3],
+				"productWeightMap": array[2]
+			}),
+			success: function (data) {
+				console.log(data);
+				$("#sureActivateConfirm").html("Please wait!");
+				setTimeout(function () {
+					$(".circle-loader").addClass("load-complete");
+
+					$('#tickActivateConfirm').show();
+
+					$("#sureActivateConfirm").html(`Criteria ${flag ? "Updated" : "Set"}`);
+				}, 900);
+
+				setTimeout(function () {
+					$("#myModalCriteriaConfirm").modal('hide');
+					document.getElementById('modalCriteriaCancelConfirm').disabled = false;
+					document.getElementById('modalCriteriaSetConfirm').disabled = false;
+				}, 2000);
+			},
+			error: function (data) {
+				document.getElementById('modalCriteriaCancelConfirm').disabled = false;
+				document.getElementById('modalCriteriaSetConfirm').disabled = false;
+				let ob = Object.keys(data);
+				let modalErr = document.querySelector('#myModalWrongDManCreate p');
+				if (ob[17] == "responseJSON") {
+					modalErr.innerHTML = data.responseJSON.errorMessage;
+				}
+				else {
+					modalErr.innerHTML = "Please Wait! We are working!";
+				}
+				$('#myModalCriteriaConfirm').modal('hide');
+				setTimeout(() => {
+					$('#myModalWrongDManCreate').modal('show');
+				}, 0);
+			}
+		});
 });
