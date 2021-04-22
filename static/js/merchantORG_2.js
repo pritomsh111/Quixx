36fc89da356f2547e677c91dcf6b613606f727ef
Pritom <pritomcsedu.56@gmail.com>
2021-04-22 07:22:20 +0600

    --- Auto-Git Commit ---

--- static/js/merchantORG_2.js
+++ static/js/merchantORG_2.js
@@ -270,7 +270,7 @@ function merchantActivateFunction(dataGet, table) {
 					row.find('td').eq(8)[0].children[0].disabled = dataOTP.data.indexOf(dataGet.data[index].approved_merchant_id) !== -1 ? true : false;
 					row.find('td').eq(9)[0].children[0].disabled = !row.find('td').eq(8)[0].children[0].disabled;
 
-					row.find('td').eq(7)[0].children[0].disabled = flag ? false : true;
+					row.find('td').eq(7)[0].children[0].disabled = criteriaEnabled ? false : true;
 
 					// console.log(element, row, statusElement, row.find('td'));
 					// var isChecked = statusElement.prop('checked');
@@ -1270,7 +1270,6 @@ var fillInputDetails = (types, values = undefined) => {
 	div.append(dummyDivFlex);
 }
 function getData() {
-
 	Array.from(document.querySelectorAll(".flexIt2 input")).map(item => {
 		item.value = "";
 	});
