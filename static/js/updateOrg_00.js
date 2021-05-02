6b724c858b212f9d10ec3e6f6e9e053080235727
Pritom <pritomcsedu.56@gmail.com>
2021-05-02 19:06:12 +0600

    --- Auto-Git Commit ---

--- static/js/updateOrg_00.js
+++ static/js/updateOrg_00.js
@@ -1,6 +1,16 @@
-function one1(){
-	setTimeout(function() { $('#loaderMama5').fadeOut(); }, 2100);
+function one1() {
+	setTimeout(function () { $('#loaderMama5').fadeOut(); }, 2100);
 }
-function two2(){
-	setTimeout(function() { $('div#hideKoro5').removeClass("hidden");$('#hideKoro5').fadeIn(); }, 2700);
+function two2() {
+	setTimeout(function () { $('div#hideKoro5').removeClass("hidden"); $('#hideKoro4').fadeIn(); }, 2900);
+}
+function three3() {
+	setTimeout(function () {
+		document.getElementById('uporg1').disabled = true;
+		document.getElementById('uporg1').style.fontSize = '14.5px';
+		document.getElementById('uporg2').disabled = false;
+		document.getElementById('uporg2').style.fontSize = '13px';
+		$('#mapForm').show();
+		$('#passForm').hide();
+	}, 3700);
 }
