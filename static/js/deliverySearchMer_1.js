95a4579a41cd6b9a07a169cb6116880801e1b340
Pritom <pritomcsedu.56@gmail.com>
2021-05-10 22:35:04 +0600

    --- Auto-Git Commit ---

--- static/js/deliverySearchMer_1.js
+++ static/js/deliverySearchMer_1.js
@@ -267,6 +267,24 @@ $('#criterionSubmit').on('click', function (eventx) {
 	}
 	if (cri != "") {
 		var tableX = $('#dtBasicExampledC').DataTable({
+			responsive: {
+				details: {
+					renderer: function (api, rowIdx, columns) {
+						var data = $.map(columns, function (col, i) {
+							return col.hidden ?
+								'<tr style="text-align:left" data-dt-row="' + col.rowIndex + '" data-dt-column="' + col.columnIndex + '">' +
+								'<td><strong>' + col.title + ':' + '</strong></td> ' +
+								'<td>' + col.data + '</td>' +
+								'</tr>' :
+								'';
+						}).join('');
+
+						return data ?
+							$('<table/>').append(data) :
+							false;
+					}
+				}
+			},
 			"processing": true,
 			'language': {
 				'loadingRecords': '&nbsp;',
@@ -300,28 +318,15 @@ $('#criterionSubmit').on('click', function (eventx) {
 				}
 			},
 			"columns": [
-				{ "targets": 1, "data": "delivery_Id" },
-				{ "targets": 1, "data": "delivery_created_date" },
-				{ "targets": 3, "data": "delivery_created_by_name" },
-				{ "targets": 4, "data": "delivery_created_by_role" },
-				{ "targets": 2, "data": "pickup_time" },
-				{ "targets": 7, "data": "sender_name" },
-				{ "targets": 8, "data": "sender_phone_number" },
-				{ "targets": 9, "data": "sender_address" },
-				{ "targets": 10, "data": "receiver_name" },
-				{ "targets": 11, "data": "receiver_phone_number" },
-				{ "targets": 12, "data": "delivery_area" },
-				{ "targets": 13, "data": "receiver_address" },
-				{ "targets": 14, "data": "delivery_type" },
-				{ "targets": 15, "data": "product_name" },
-				{ "targets": 16, "data": "product_qty" },
-				{ "targets": 17, "data": "product_cost" },
-				{ "targets": 18, "data": "delivery_charge" },
-				{ "targets": 19, "data": "payment_method" },
-				{ "targets": 19, "data": "assign_delivery_man_id" },
-				{ "targets": 19, "data": "assign_delivery_man_name" },
-				{ "targets": 19, "data": "assign_delivery_man_phone" },
-				{ "targets": 19, "data": "delivery_complete_date" },
+				{ "targets": 100, "data": null, "defaultContent": "" },
+				{ "targets": 0, "data": "delivery_Id" },
+				{ "targets": 2, "data": "delivery_status" },
+				{
+					"targets": 5, "data": null, render: function (data, type, row) {
+
+						return row.assign_delivery_man_name + ", " + row.assign_delivery_man_phone;
+					}
+				},
 				{
 					"targets": 19, "data": "transaction_complete", render: function (data, type, row) {
 						if (row.transaction_complete == false) {
@@ -336,7 +341,79 @@ $('#criterionSubmit').on('click', function (eventx) {
 					}
 				},
 				{ "targets": 19, "data": "transaction_complete_date" },
-				{ "targets": 23, "data": "delivery_note" }
+				{ "targets": 15, "data": "product_name" },
+				{ "targets": 16, "data": "product_qty" },
+				{ "targets": 17, "data": "product_cost" },
+				{ "targets": 18, "data": "delivery_charge" },
+				{ "targets": 19, "data": "payment_method" },
+				{ "targets": 19, "data": "delivery_complete_date" },
+				{ "targets": 1, "data": "delivery_created_date" },
+				{ "targets": 10, "data": "receiver_name" },
+				{ "targets": 11, "data": "receiver_phone_number" },
+				{
+					"targets": 24, "data": "delivery_city", render: function (data, type, row) {
+						let a = row.delivery_city;
+						return a ? row.delivery_city : "";
+					}
+				},
+				{ "targets": 123, "data": "delivery_area" },
+				{ "targets": 13, "data": "receiver_address" },
+				{
+					"targets": 25, "data": "delivery_product_type", render: function (data, type, row) {
+						let a = row.delivery_product_type;
+						if (a) {
+							a = a?.includes("delivery_product_type_na") ? "NOT_SELECTED" : a;
+							return a;
+						}
+						return "---";
+					}
+				},
+				{
+					"targets": 27, "data": "delivery_weight", render: function (data, type, row) {
+						let a = row.delivery_weight;
+						if (a) {
+							a = a?.includes("delivery_weight_na") ? "NOT_SELECTED" : a + "KG";
+							return a;
+						}
+						return "---";
+					}
+				},
+				{
+					"targets": 26, "data": "delivery_day_type", render: function (data, type, row) {
+						let a = row.delivery_day_type;
+						if (a) {
+							a = a?.includes("delivery_day_type_na") ? "NOT_SELECTED" : a;
+							return a;
+						}
+						return "---";
+					}
+				},
+				{
+					"targets": 28, "data": "delivery_distance", render: function (data, type, row) {
+						let a = row.delivery_distance;
+						if (a) {
+							a = a?.includes("delivery_distance_na") ? "NOT_SELECTED" : a + "KM";
+							return a;
+						}
+						return "---";
+					}
+				},
+				{
+					"targets": 29, "data": "delivery_city_criteria", render: function (data, type, row) {
+						let a = row.delivery_city_criteria;
+						if (a) {
+							a = a?.includes("delivery_city_criteria_na") ? "NOT_SELECTED" : a;
+							return a;
+						}
+						return "---";
+					}
+				},
+				{ "targets": 14, "data": "delivery_type" },
+				{ "targets": 5, "data": "pickup_time" },
+				{ "targets": 20, "data": "delivery_note" },
+				{ "targets": 7, "data": "sender_name" },
+				{ "targets": 8, "data": "sender_phone_number" },
+				{ "targets": 9, "data": "sender_address" },
 			]
 		});
 		tableX.clear().draw();
