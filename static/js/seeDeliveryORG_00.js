c9f2d772bbf688ebb581da53b62664515faad08d
Pritom <pritomcsedu.56@gmail.com>
2021-05-13 21:29:05 +0600

    --- Auto-Git Commit ---

--- static/js/seeDeliveryORG_00.js
+++ static/js/seeDeliveryORG_00.js
@@ -1,16 +1,16 @@
-function one1(){
-	setTimeout(function() { $('#loaderMama4').fadeOut(); }, 2100);
+function one1() {
+	setTimeout(function () { $('#loaderMama4').fadeOut(); }, 2100);
 }
-function two2(){
-	setTimeout(function() { $('div#hideKoro4').removeClass("hidden");$('#hideKoro4').fadeIn(); }, 2900);
+function two2() {
+	setTimeout(function () { $('div#hideKoro4').removeClass("hidden"); $('#hideKoro4').fadeIn(); }, 2900);
 }
-function three3(){
-	setTimeout(function() {
+function three3() {
+	setTimeout(function () {
 		document.getElementById('onec').disabled = false;
 		document.getElementById('twoc').disabled = true;
 		document.getElementById('twoc').style.fontSize = '14.5px';
 		document.getElementById('onec').style.fontSize = '13px';
-		document.getElementById('onec').innerHTML = 'Details of Delivery and Delivery Man';
+		document.getElementById('onec').innerHTML = 'Details of Delivery Man';
 		$('#dtBasicExample').hide();
 		$('.a').hide();
 		$('#map').show();
