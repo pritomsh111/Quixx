eee7ad2bd686ee97ffaae7e41a37b7e25e452097
Pritom <pritomcsedu.56@gmail.com>
2021-05-21 19:27:28 +0600

    --- Auto-Git Commit ---

--- tests/app.js
+++ tests/app.js
@@ -1,8 +1,8 @@
 var data2 = [
-    [3, 'Cheese', true],
-    [1, 'Apples', true],
-    [2, 'Carrots', true],
-    [1, 'Oranges', false],
+    [3, true, 'Cheese'],
+    [1, true, 'Apples'],
+    [2, true, 'Carrots'],
+    [1, false, 'Oranges'],
 ];
 var filterOptions = function (o, cell, x, y, value, config) {
     var value = o.getValueFromCoords(x - 1, y);
@@ -22,14 +22,14 @@ spreadsheet = jspreadsheet(document.getElementById('spreadsheet2'), {
     data: data2,
     columns: [
         { type: 'dropdown', title: 'Category', width: '300', source: [{ 'id': '1', 'name': 'Fruits' }, { 'id': '2', 'name': 'Legumes' }, { 'id': '3', 'name': 'General Food' }] },
-        { type: 'dropdown', title: 'Food', width: '200', source: ['Apples', 'Bananas', 'Carrots', 'Oranges', 'Cheese'], filterOptions: filterOptions },
         { type: 'checkbox', title: 'Buy', width: '100' },
+        { type: 'dropdown', title: 'Food', width: '200', source: ['Apples', 'Bananas', 'Carrots', 'Oranges', 'Cheese'], filterOptions: filterOptions },
     ],
     onchange: function (instance, cell, c, r, value) {
         if (c == 0) {
             var columnName = jspreadsheet.getColumnNameFromId([c + 1, r]);
             console.log(instance, cell, c, r, value, columnName);
-            instance.jexcel.setValue(columnName, '');
+            instance.jexcel.setValue(columnName);
         }
     },
     license: 'NDc4ZDc3NWJiNjIzNzU4MTVmNjA4MjdlYjk1MjlmZDZiYzk4MWM3ZmM2OTdiZDczOWQ2MGRkZjdhNTc4ODExNmMyMDZkZDIwNzExYzJmM2EwOTBiYWUxM2M0NGY1YzNiZmVmZTY1OGY5OWI2ZDg2NDMyNjgwOTFhZGQ2MzVlYmEsZXlKdVlXMWxJam9pY0dGMWJDNW9iMlJsYkNJc0ltUmhkR1VpT2pFMk5EY3pPRGc0TURBc0ltUnZiV0ZwYmlJNld5SnFjMmhsYkd3dWJtVjBJaXdpWTI5a1pYTmhibVJpYjNndVkyOXRJaXdpYW5Od2NtVmhaSE5vWldWMExtTnZiU0lzSW1OellpNWhjSEFpTENKalpIQnVMbWx2SWl3aWJHOWpZV3hvYjNOMElsMHNJbkJzWVc0aU9pSXlJbjA9',
