908f654d1081a03e7878dbf94e07985889e6d2ef
Pritom <pritomcsedu.56@gmail.com>
2021-06-02 12:30:08 +0600

    --- Auto-Git Commit ---

--- static-landing/js/home.js
+++ static-landing/js/home.js
@@ -170,4 +170,61 @@
             document.querySelector('.video').innerHTML = "";
         }
     });
+
 })();
+
+// const body = document.body;
+// const firstSection = document.querySelector(".container");
+// const lastSection = document.querySelector(".Quixx__Pricing");
+// const table = document.querySelector("table");
+// const thead = document.querySelector("table thead");
+// const mq = window.matchMedia("(min-width: 780px)");
+// const stickyClass = "sticky-table";
+// const sticky2Class = "sticky2-table";
+
+// let tableWidth = table.offsetWidth;
+// let tableOffsetTop = table.getBoundingClientRect().top;
+// // or
+// //let tableOffsetTop = table.offsetTop;
+// let theadHeight = thead.offsetHeight;
+
+// window.addEventListener("scroll", scrollHandler);
+// window.addEventListener("resize", resizeHandler);
+
+// function scrollHandler() {
+//     if (mq.matches) {
+//         const scrollY = window.pageYOffset;
+//         const lastSectionOffsetTop = lastSection.getBoundingClientRect().top;
+//         if (scrollY >= tableOffsetTop) {
+//             thead.style.width = `${tableWidth}px`;
+//             if (lastSectionOffsetTop > theadHeight) {
+//                 firstSection.classList.remove(sticky2Class);
+//                 firstSection.classList.add(stickyClass);
+//                 thead.style.top = 0;
+//                 firstSection.style.paddingTop = `${theadHeight}px`;
+//             } else {
+//                 firstSection.classList.remove(stickyClass);
+//                 firstSection.classList.add(sticky2Class);
+//                 thead.style.top = `calc(100% - ${theadHeight}px)`;
+//             }
+//         } else {
+//             firstSection.classList.remove(stickyClass, sticky2Class);
+//             firstSection.style.paddingTop = 0;
+//             thead.style.width = "100%";
+//             thead.style.top = "auto";
+//         }
+//     }
+// }
+
+function resizeHandler() {
+    if (mq.matches) {
+        tableWidth = table.offsetWidth;
+        tableOffsetTop = firstSection.offsetHeight;
+        theadHeight = thead.offsetHeight;
+    } else {
+        body.classList.remove(stickyClass, sticky2Class);
+        body.style.paddingTop = 0;
+        thead.style.width = "100%";
+        thead.style.top = "auto";
+    }
+}
