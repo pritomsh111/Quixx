95b115ddc37b42d7b31e8f72fcf612a6b984e7f7
Pritom <pritomcsedu.56@gmail.com>
2021-06-02 13:00:18 +0600

    --- Auto-Git Commit ---

--- static-landing/js/home.js
+++ static-landing/js/home.js
@@ -173,48 +173,48 @@
 
 })();
 
-// const body = document.body;
-// const firstSection = document.querySelector(".container");
-// const lastSection = document.querySelector(".Quixx__Pricing");
-// const table = document.querySelector("table");
-// const thead = document.querySelector("table thead");
-// const mq = window.matchMedia("(min-width: 780px)");
-// const stickyClass = "sticky-table";
-// const sticky2Class = "sticky2-table";
+const body = document.body;
+const firstSection = document.querySelector(".Quixx__Panels");
+const lastSection = document.querySelector(".Quixx__Video");
+const table = document.querySelector("table");
+const thead = document.querySelector("table thead");
+const mq = window.matchMedia("(min-width: 780px)");
+const stickyClass = "sticky-table";
+const sticky2Class = "sticky2-table";
 
-// let tableWidth = table.offsetWidth;
+let tableWidth = table.offsetWidth;
 // let tableOffsetTop = table.getBoundingClientRect().top;
-// // or
-// //let tableOffsetTop = table.offsetTop;
-// let theadHeight = thead.offsetHeight;
+// or
+let tableOffsetTop = table.offsetTop;
+let theadHeight = thead.offsetHeight;
 
-// window.addEventListener("scroll", scrollHandler);
-// window.addEventListener("resize", resizeHandler);
+window.addEventListener("scroll", scrollHandler);
+window.addEventListener("resize", resizeHandler);
 
-// function scrollHandler() {
-//     if (mq.matches) {
-//         const scrollY = window.pageYOffset;
-//         const lastSectionOffsetTop = lastSection.getBoundingClientRect().top;
-//         if (scrollY >= tableOffsetTop) {
-//             thead.style.width = `${tableWidth}px`;
-//             if (lastSectionOffsetTop > theadHeight) {
-//                 firstSection.classList.remove(sticky2Class);
-//                 firstSection.classList.add(stickyClass);
-//                 thead.style.top = 0;
-//                 firstSection.style.paddingTop = `${theadHeight}px`;
-//             } else {
-//                 firstSection.classList.remove(stickyClass);
-//                 firstSection.classList.add(sticky2Class);
-//                 thead.style.top = `calc(100% - ${theadHeight}px)`;
-//             }
-//         } else {
-//             firstSection.classList.remove(stickyClass, sticky2Class);
-//             firstSection.style.paddingTop = 0;
-//             thead.style.width = "100%";
-//             thead.style.top = "auto";
-//         }
-//     }
-// }
+function scrollHandler() {
+    if (mq.matches) {
+        const scrollY = window.pageYOffset;
+        const lastSectionOffsetTop = lastSection.getBoundingClientRect().top;
+        if (scrollY >= tableOffsetTop + 650) {
+            thead.style.width = `${tableWidth}px`;
+            if (lastSectionOffsetTop > theadHeight) {
+                body.classList.remove(sticky2Class);
+                body.classList.add(stickyClass);
+                thead.style.top = 0;
+                body.style.paddingTop = `${theadHeight}px`;
+            } else {
+                body.classList.remove(stickyClass);
+                body.classList.add(sticky2Class);
+                thead.style.top = `calc(100% - ${theadHeight}px)`;
+            }
+        } else {
+            body.classList.remove(stickyClass, sticky2Class);
+            body.style.paddingTop = 0;
+            thead.style.width = "100%";
+            thead.style.top = "auto";
+        }
+    }
+}
 
 function resizeHandler() {
     if (mq.matches) {
