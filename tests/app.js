6452888786bca5822679cc3d266df50d13757c0b
Pritom <pritomcsedu.56@gmail.com>
2021-07-09 01:48:40 +0600

    --- Auto-Git Commit ---

--- tests/app.js
+++ tests/app.js
@@ -3,3 +3,14 @@
 // console.log(document.querySelector("#text").getTotalLength());
 
 console.log(window.screen);
+
+document.querySelector("div").addEventListener("click", function () {
+    if (document.querySelector("svg>circle").classList.contains("one")) {
+        document.querySelector("svg>circle").classList.remove("one");
+        document.querySelector("svg>circle").classList.add("two");
+    }
+    else {
+        document.querySelector("svg>circle").classList.add("one");
+        document.querySelector("svg>circle").classList.remove("two");
+    }
+});
