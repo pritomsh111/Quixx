7237b95a1c5a7624b75e8af61fa2930068e44f53
Pritom <pritomcsedu.56@gmail.com>
2021-07-23 03:46:42 +0600

    --- Auto-Git Commit ---

--- tests/GSAP/app.js
+++ tests/GSAP/app.js
@@ -1,1 +1,10 @@
 gsap.to("#recx", { x: 400, scale: 1.2, rotation: 60, duration: 3, xPercent: -300 });
+
+
+let a = Object.create({}, { one: { value: 3, enumerable: true }, two: { value: 4 } });
+
+console.log(a);
+
+for (const key in a) {
+    console.log(key);
+}
