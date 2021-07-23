a7b799033a0d4c110b4825ec2b8f61de211f526e
Pritom <pritomcsedu.56@gmail.com>
2021-07-24 00:51:09 +0600

    --- Auto-Git Commit ---

--- tests/GSAP/app.js
+++ tests/GSAP/app.js
@@ -1,4 +1,6 @@
-gsap.to("#recx", { x: 400, scale: 1.2, rotation: 60, duration: 3, xPercent: -300 });
+gsap.to("#recx", { x: 200, scale: 1.2, rotation: 90, duration: 3, xPercent: 100 });
+gsap.to("#recx", { strokeWidth: 20 });
+gsap.to("#recx", { strokeDashoffset: 800, duration: 10, yoyo: true, repeat: -1 });
 
 
 // let a = Object.create({}, { one: { value: 3, enumerable: true }, two: { value: 4 } });
@@ -10,32 +12,32 @@ gsap.to("#recx", { x: 400, scale: 1.2, rotation: 60, duration: 3, xPercent: -300
 // }
 // console.log(b);
 
-function Person() {
-    this.name = "Hello"
-}
+// function Person() {
+//     this.name = "Hello"
+// }
 
-let a = new Person();
+// let a = new Person();
 
-Person.prototype.age = 10;
+// Person.prototype.age = 10;
 
-let b = Object.create(a);
+// let b = Object.create(a);
 
-Array.prototype.hello = 100;
+// Array.prototype.hello = 100;
 
-let arr = [12, 12, 12];
+// let arr = [12, 12, 12];
 
-class Hello {
-    omg = "Hello";
-}
+// class Hello {
+//     omg = "Hello";
+// }
 
-Hello.prototype.ass = () => {
-    console.log(this);
-}
+// Hello.prototype.ass = () => {
+//     console.log(this);
+// }
 
-let h = new Hello();
+// let h = new Hello();
 
-class pelllo extends Hello {
+// class pelllo extends Hello {
 
-}
+// }
 
-let p = new pelllo();
+// let p = new pelllo();
