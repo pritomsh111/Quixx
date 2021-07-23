e3474fbd777f2f5049ce440c98b2079a2b5dfaff
Pritom <pritomcsedu.56@gmail.com>
2021-07-23 17:49:39 +0600

    --- Auto-Git Commit ---

--- tests/GSAP/app.js
+++ tests/GSAP/app.js
@@ -9,6 +9,33 @@ gsap.to("#recx", { x: 400, scale: 1.2, rotation: 60, duration: 3, xPercent: -300
 //     console.log(key);
 // }
 // console.log(b);
-let a = { v: 3, b: function () { } }
 
-console.log(a);
+function Person() {
+    this.name = "Hello"
+}
+
+let a = new Person();
+
+Person.prototype.age = 10;
+
+let b = Object.create(a);
+
+Array.prototype.hello = 100;
+
+let arr = [12, 12, 12];
+
+class Hello {
+    omg = "Hello";
+}
+
+Hello.prototype.ass = () => {
+    console.log(this);
+}
+
+let h = new Hello();
+
+class pelllo extends Hello {
+
+}
+
+let p = new pelllo();
