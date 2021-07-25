86cb72eab1f8ed0d82d36c697bb2a66c76b77ceb
Pritom <pritomcsedu.56@gmail.com>
2021-07-25 09:28:16 +0600

    --- Auto-Git Commit ---

--- tests/GSAP/app.js
+++ tests/GSAP/app.js
@@ -2,6 +2,12 @@ gsap.to("#recx", { x: 200, scale: 1.2, rotation: 90, duration: 3, xPercent: 100 
 gsap.to("#recx", { strokeWidth: 20 });
 gsap.to("#recx", { strokeDasharray: 0, duration: 10, yoyo: true, repeat: -1 });
 
+window.addEventListener("load", function () {
+    console.log("load");
+});
+window.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM");
+});
 function _32() {
     console.log("HEllo");
 }
