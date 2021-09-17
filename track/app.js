550ec561cc10f3b5f0180d123913553976c7ec09
Pritom <pritomcsedu.56@gmail.com>
2021-09-17 17:38:04 +0600

    --- Auto-Git Commit ---

--- track/app.js
+++ track/app.js
@@ -2,7 +2,8 @@ axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
 let tick = document.querySelector(".delivery__progress");
 let path = document.querySelectorAll(".delivery__progress>div");
-let ret_can_hold = document.querySelector(".delivery__progress").children[4].children[0];
+let ret_can_hold = document.querySelector(".delivery__progress").children[4]
+    .children[0];
 let trackBtn = document.querySelector("#trackBtn");
 let trackId = document.querySelector("#trackID");
 let errorSpan = document.querySelector(".errorInput>span");
@@ -12,17 +13,16 @@ let interval;
 
 trackId.addEventListener("keyup", function (event) {
     if (event.target.value) {
-        trackBtn.classList.add('show-off');
+        trackBtn.classList.add("show-off");
         errorSpan.innerHTML = "";
-    }
-    else {
-        trackBtn.classList.remove('show-off');
+    } else {
+        trackBtn.classList.remove("show-off");
     }
 });
 trackBtn.addEventListener("click", function (event) {
     let input = trackId.value;
     trackId.value = "";
-    trackBtn.classList.remove('show-off');
+    trackBtn.classList.remove("show-off");
     httpFuncGet(input);
     if (interval) {
         clearInterval(interval);
@@ -38,16 +38,15 @@ async function httpFuncGet(input) {
         method: "GET",
         data: input,
         url: "/posts",
-    }
+    };
     try {
         let result = await axios(params);
-        trackDelivery.classList.add('show-off');
+        trackDelivery.classList.add("show-off");
         trackDeliveryH2.innerHTML = "Tracking Details";
         progressFull();
         // console.log(result);
-    }
-    catch {
-        errorSpan.innerHTML = "Wrong Track ID!"
+    } catch {
+        errorSpan.innerHTML = "Wrong Track ID!";
     }
 }
 
@@ -63,7 +62,10 @@ const status = {
 };
 function progressFull() {
     let result = "enroute_to_delivery".toLowerCase();
-    document.querySelector(".delivery__status").innerHTML = result.replace(/_/g, " ");
+    document.querySelector(".delivery__status").innerHTML = result.replace(
+        /_/g,
+        " "
+    );
     let index = Object.keys(status).indexOf(result);
 
     for (let i = 0; i <= index; i++) {
@@ -72,9 +74,11 @@ function progressFull() {
                 ? (ret_can_hold.innerHTML = "&#11152")
                 : result === "cancelled"
                     ? (ret_can_hold.innerHTML = "&#128473")
-                    : result === "on_hold" ? (ret_can_hold.innerHTML = "&#33;") : i = 10;
+                    : result === "on_hold"
+                        ? (ret_can_hold.innerHTML = "&#33;")
+                        : (i = 10);
             path[i - 1].classList.add("ret_can_hold");
-            tick.children[i - 1].children[0].classList.add("ret_can_hold")
+            tick.children[i - 1].children[0].classList.add("ret_can_hold");
             path[i - 2].classList.remove("complete_path");
             break;
         }
@@ -87,30 +91,54 @@ function progressFull() {
     index < 4 && path[index + 1].classList.add("next_delivery_status");
 
     let dateString = "2021-04-07";
-    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
     var d = new Date(dateString);
     var dayName = days[d.getDay()];
 
-    document.querySelector(".delivery__details>div:nth-child(1)>h2:first-of-type").innerHTML = dayName;
-    document.querySelector(".delivery__details>div:nth-child(1)>h2:last-of-type").innerHTML = dateString.replace(/-/g, "/");
+    document.querySelector(
+        ".delivery__details>div:nth-child(1)>h2:first-of-type"
+    ).innerHTML = dayName;
+    document.querySelector(
+        ".delivery__details>div:nth-child(1)>h2:last-of-type"
+    ).innerHTML = dateString.replace(/-/g, "/");
 
-    document.querySelector(".delivered__place").children[0].children[0].innerHTML = `<strong>${new Date(2021, 11, 24, 10, 33, 30)}</strong>`;
-    document.querySelector(".delivery__location>p").innerHTML = "<strong>Cumilla</strong>";
+    document.querySelector(
+        ".delivered__place"
+    ).children[0].children[0].innerHTML = `<strong>${new Date(
+        2021,
+        11,
+        24,
+        10,
+        33,
+        30
+    )}</strong>`;
+    document.querySelector(".delivery__location>p").innerHTML =
+        "<strong>Cumilla</strong>";
 
-    let tbody = document.querySelector(".shipment__progress__details table tbody");
+    let tbody = document.querySelector(
+        ".shipment__progress__details table tbody"
+    );
 
     const shipment = {
         just_created: {
-            date: "2019-21-32 20:30"
+            date: "2019-21-32 20:30",
         },
         enroute_to_pickup: {
-            date: "2019-21-32 13:30"
+            date: "2019-21-32 13:30",
         },
         picked_up: {
-            date: "2019-21-32 14:30"
+            date: "2019-21-32 14:30",
         },
         enroute_to_delivery: {
-            date: "2019-21-32 15:30"
+            date: "2019-21-32 15:30",
         },
         // delivered: {
         //     date: "2019-21-32 16:30"
@@ -124,10 +152,10 @@ function progressFull() {
         // on_hold: {
         //     date: "2019-21-32 18:30"
         // },
-    }
+    };
     let newRow;
     if (tbody.rows.length) {
-        Array.from(tbody.rows).map(row => {
+        Array.from(tbody.rows).map((row) => {
             row.remove();
         });
     }
@@ -135,21 +163,19 @@ function progressFull() {
     Object.keys(shipment).map((item, index) => {
         newRow = tbody.insertRow(0);
         newRow.style.cssText = `--i:${index}`;
-        Object.keys(shipment[item]).map(td => {
+        Object.keys(shipment[item]).map((td) => {
             if (item === "delivered") {
-                newRow.insertCell(0).innerHTML = '<div style="--j:' + index + '" class="check-wrap"></div>';
-            }
-            else if (item === "returned") {
+                newRow.insertCell(0).innerHTML =
+                    '<div style="--j:' + index + '" class="check-wrap"></div>';
+            } else if (item === "returned") {
                 newRow.insertCell(0).innerHTML = "<div class='returnedx'>&#11152</div>";
-            }
-            else if (item === "cancelled") {
+            } else if (item === "cancelled") {
                 newRow.insertCell(0).innerHTML = "<div class='cancelledx'>X</div>";
-            }
-            else if (item === "on_hold") {
+            } else if (item === "on_hold") {
                 newRow.insertCell(0).innerHTML = "<div class='on_holdx'>&#33;</div>";
-            }
-            else if (index === Object.keys(shipment).length - 1) {
-                newRow.insertCell(0).innerHTML = '<div style="--j:' + index + '" class="check-wrap"></div>';
+            } else if (index === Object.keys(shipment).length - 1) {
+                newRow.insertCell(0).innerHTML =
+                    '<div style="--j:' + index + '" class="check-wrap"></div>';
                 newRow.insertCell(1).innerHTML = item.toUpperCase().replace(/_/g, " ");
                 newRow.insertCell(2).innerHTML = shipment[item][td];
 
@@ -157,13 +183,21 @@ function progressFull() {
 
                 newRow.style.cssText = `--i:${index + 1}`;
 
-                newRow.insertCell(0).innerHTML = '<div style="--j:' + index + '" class="snippet" data-title=".dot-stretching"><div class="stage"><div class="dot-stretching"></div></div></div>';
-                newRow.insertCell(1).innerHTML = Object.keys(status)[Object.keys(shipment).length].toUpperCase().replace(/_/g, " ");
-                newRow.insertCell(2).innerHTML = '<div style="--j:' + index + '" class="snippet" data-title=".dot-stretching"><div class="stage"><div class="dot-stretching"></div></div></div>';
+                newRow.insertCell(0).innerHTML =
+                    '<div style="--j:' +
+                    index +
+                    '" class="snippet" data-title=".dot-stretching"><div class="stage"><div class="dot-stretching"></div></div></div>';
+                newRow.insertCell(1).innerHTML = Object.keys(status)
+                [Object.keys(shipment).length].toUpperCase()
+                    .replace(/_/g, " ");
+                newRow.insertCell(2).innerHTML =
+                    '<div style="--j:' +
+                    index +
+                    '" class="snippet" data-title=".dot-stretching"><div class="stage"><div class="dot-stretching"></div></div></div>';
                 return;
-            }
-            else {
-                newRow.insertCell(0).innerHTML = '<div style="--j:' + index + '" class="check-wrap"></div>';
+            } else {
+                newRow.insertCell(0).innerHTML =
+                    '<div style="--j:' + index + '" class="check-wrap"></div>';
             }
             newRow.insertCell(1).innerHTML = item.toUpperCase().replace(/_/g, "  ");
             newRow.insertCell(2).innerHTML = shipment[item][td];
@@ -173,32 +207,50 @@ function progressFull() {
 
 // Shipment Progress visibility clickHandler
 
-document.querySelector(".shipment__progress").addEventListener("click", function () {
-    this.classList.toggle("active");
-    if (window.innerWidth < 360) {
-        if (this.classList.contains("active"))
-            document.querySelector(".shipment__details").style.marginTop = "17rem";
-        else
-            document.querySelector(".shipment__details").style.marginTop = "0rem";
-    }
-    else if (window.innerWidth < 820) {
-        if (this.classList.contains("active"))
-            document.querySelector(".shipment__details").style.marginTop = "22rem";
-        else
-            document.querySelector(".shipment__details").style.marginTop = "0rem";
-    }
-    setTimeout(() => {
-        window.scrollTo(0, document.body.scrollHeight);
-    }, 350);
-});
-
+document
+    .querySelector(".shipment__progress")
+    .addEventListener("click", function () {
+        this.classList.toggle("active");
+        if (window.innerWidth < 360) {
+            if (this.classList.contains("active"))
+                document.querySelector(".shipment__details").style.marginTop = "17rem";
+            else
+                document.querySelector(".shipment__details").style.marginTop = "0rem";
+        } else if (window.innerWidth < 820) {
+            if (this.classList.contains("active"))
+                document.querySelector(".shipment__details").style.marginTop = "22rem";
+            else
+                document.querySelector(".shipment__details").style.marginTop = "0rem";
+        }
+        setTimeout(() => {
+            window.scrollTo(0, document.body.scrollHeight);
+        }, 350);
+    });
 
 // Shipment Details - Product
 function shipmentDetails2(e) {
-    e.target.innerHTML = e.target.innerHTML.trim() === "Show Less -" ? "Show More +" : e.target.innerHTML.trim() === "Show More +" ? "Show Less -" : null;
-    document.querySelector(".shipment__details .shipment__details__products ul:not(:first-child)").classList.toggle("show__list");
+    e.target.innerHTML =
+        e.target.innerHTML.trim() === "Show Less -"
+            ? "Show More +"
+            : e.target.innerHTML.trim() === "Show More +"
+                ? "Show Less -"
+                : null;
+    document
+        .querySelector(
+            ".shipment__details .shipment__details__products ul:not(:first-child)"
+        )
+        .classList.toggle("show__list");
 }
 
-document.querySelector(".shipment__details .shipment__details__products>ul>li:last-child>span:last-child")
+document
+    .querySelector(
+        ".shipment__details .shipment__details__products>ul>li:last-child>span:last-child"
+    )
     .addEventListener("click", shipmentDetails2);
 
+if (location.search) {
+    let search = new URLSearchParams(location.search);
+
+    for (let [key, value] of search.entries()) {
+    }
+}
