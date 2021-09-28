cea0ce5c42c26f736529489aba47f8ac97030f26
Pritom <pritomcsedu.56@gmail.com>
2021-09-28 22:39:59 +0600

    --- Auto-Git Commit ---

--- test2/app.js
+++ test2/app.js
@@ -1,6 +1,10 @@
-document.querySelector(".button").addEventListener("click", function () {
-    this.classList.add("active");
-    setTimeout(() => {
-        this.classList.remove("active");
-    }, 4250);
-});
+// document.querySelector(".button").addEventListener("click", function () {
+//     this.classList.add("active");
+//     setTimeout(() => {
+//         this.classList.remove("active");
+//     }, 4250);
+// });
+
+let tl = gsap.timeline();
+
+tl.to(".button", { padding: "2rem" })
