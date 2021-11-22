d2e62dc08c8031484470f5dad4e07be18c7a4edb
Pritom <pritomcsedu.56@gmail.com>
2021-11-23 01:37:56 +0600

    --- Auto-Git Commit ---

--- Leetcode/9. Palindrome Number.js
+++ Leetcode/9. Palindrome Number.js
@@ -3,29 +3,43 @@
 // y = y.split("").reverse().join("");
 // console.log(y === (x + ""));
 
-let x = 11011;
-let count = 0, j = 0;
-let remainder = [];
+// let x = 110;
+// let count = 0, j = 0;
+// let remainder = [];
+// if (x < 0) {
+//     // return "false";
+// }
+// else {
+//     while (x > 0) {
+//         remainder[count++] = x % 10;
+//         x = parseInt(x / 10);
+//     }
+//     console.log(remainder);
+//     for (let i = 0; i < Math.floor(count / 2); i++) {
+//         if (remainder[i] === remainder[count - 1 - i]) {
+//             j++;
+//         }
+//     }
+//     if (j === Math.floor(count / 2)) {
+//         console.log("true");
+//         // return true;
+//     }
+//     else {
+//         console.log("false");
+//         // return false;
+//     }
+// }
+
+let x = 123211;
+let copy = x;
+let reverse = 0;
 if (x < 0) {
-    // return "false";
+    // return false;
 }
 else {
-    while (x > 0) {
-        remainder[count++] = x % 10;
-        x = parseInt(x / 10);
+    while (copy > 0) {
+        reverse = (reverse * 10) + (copy % 10);
+        copy = Math.floor(copy / 10);
     }
-    console.log(remainder);
-    for (let i = 0; i < count / 2; i++) {
-        if (remainder[i] === remainder[count - 1 - i]) {
-            j++;
-        }
-    }
-    if (j === count / 2) {
-        console.log("true");
-        // return true;
-    }
-    else {
-        console.log("false");
-        // return false;
-    }
+    console.log(reverse === x);
 }
