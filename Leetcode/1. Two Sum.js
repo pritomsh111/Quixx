74141d725faecbab867b260d2c701933e0164412
Pritom <pritomcsedu.56@gmail.com>
2021-11-22 20:06:44 +0600

    --- Auto-Git Commit ---

--- Leetcode/1. Two Sum.js
+++ Leetcode/1. Two Sum.js
@@ -1,4 +1,6 @@
 let numbers = [1, 2, 1, 4];
+let target = 5;
+let resultArray = [];
 let map = new Map();
 numbers.map((item, index) => {
     if (!map.has(item)) {
@@ -9,5 +11,16 @@ numbers.map((item, index) => {
     }
 });
 
+numbers.map((item, index) => {
+    let number1 = target - item;
+    if (map.has(number1)) {
+        if (index === map.get(number1)[0]) {
+            resultArray.concat(index).concat(map.get(number1)[1]);
+        }
+        else {
+            resultArray.concat(index).concat(map.get(number1)[0]);
+        }
+    }
+});
 
-console.log(map);
+console.log(resultArray);
