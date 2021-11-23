4d01a52c5a8c7da86d6216eaf867a61ef1b4eecf
Pritom <pritomcsedu.56@gmail.com>
2021-11-23 13:40:33 +0600

    --- Auto-Git Commit ---

--- Leetcode/14. Longest Common Prefix.js
+++ Leetcode/14. Longest Common Prefix.js
@@ -1,3 +1,11 @@
 let map = new Map();
 let strs = ["flower", "flow", "flight"];
-strs.map((item) => { });
+
+strs.sort((a, b) => a.length - b.length);
+
+console.log(strs);
+// strs.some((item) => {
+//     [...item].some((innerItem, index) => {
+
+//     });
+// });
