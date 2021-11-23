5fce1e6fea33bd7609cca371fe6bc5fb5896f332
Pritom <pritomcsedu.56@gmail.com>
2021-11-23 21:45:38 +0600

    --- Auto-Git Commit ---

--- Leetcode/20. Valid Parentheses.js
+++ Leetcode/20. Valid Parentheses.js
@@ -1,13 +1,20 @@
-let s = "()({})([])(())";
+let s = "()[]{}";
+// let s = "(()(){)";
+s = [...s];
 let a = [], prev;
-for (let i = 0; i < string.length; i++) {
+for (let i = 0; i < s.length; i++) {
     if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
         a.push(s[i]);
         prev = s[i];
     }
     else {
         if ((prev === "(" && s[i] === ")") || (prev === "[" && s[i] === "]") || (prev === "{" && s[i] === "}")) {
-            a.slice(index - 1, 1);
+            a.splice(i - 1, 1);
+            s.splice(i - 1, 2);
+            if (i - 2 >= 0) {
+                prev = a[i - 2];
+                i -= 2;
+            }
         }
         else {
             break;
