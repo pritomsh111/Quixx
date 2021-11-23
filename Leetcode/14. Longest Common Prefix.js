077a51b63cbc75b5054e0ec9ec98dd10eda8397a
Pritom <pritomcsedu.56@gmail.com>
2021-11-23 16:11:09 +0600

    --- Auto-Git Commit ---

--- Leetcode/14. Longest Common Prefix.js
+++ Leetcode/14. Longest Common Prefix.js
@@ -1,19 +1,26 @@
-let strs = ["cir", "car"];
+let strs = ["acc", "aaa", "aaba"];
 let l1 = strs.length - 1;
-let l2, count = 0, str = '';
-for (let i = 0; i < l1; i++) {
-    count = 0;
-    str = '';
-    l2 = strs[i].length < strs[i + 1].length ? strs[i].length : strs[i + 1].length;
-    for (let j = 0; j < l2; j++) {
-        if (strs[i][j] === strs[i + 1][j]) {
-            count++;
-            strs = strs[i][j];
-        }
-        else {
-            break;
+if (l1 === 0) {
+    return strs[0];
+}
+else {
+    let l2, count = 0, str = '';
+    for (let i = 0; i < l1; i++) {
+        l2 = strs[i].length < strs[i + 1].length ? strs[i].length : strs[i + 1].length;
+        // count ? l2 = count : null;
+        count = 0;
+        str = '';
+        for (let j = 0; j < l2; j++) {
+            if (strs[i][j] === strs[i + 1][j]) {
+                count++;
+                str += strs[i][j];
+            }
+            else {
+                break;
+            }
         }
     }
-}
 
-console.log(count, str);
+    console.log(count, str);
+
+}
