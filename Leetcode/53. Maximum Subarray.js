306d97583215c723cd99d8e0130376a3fc16f7c3
Pritom <pritomcsedu.56@gmail.com>
2021-11-25 12:24:10 +0600

    --- Auto-Git Commit ---

--- Leetcode/53. Maximum Subarray.js
+++ Leetcode/53. Maximum Subarray.js
@@ -1,12 +1,15 @@
-let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-let sum = 0;
+let nums = [5, 4, -1, 7, 8];
+let sum = 0, max = -10000;
 nums.map(item => {
     if (sum + item >= 0) {
         sum += item;
+        if (sum > max) {
+            max = sum;
+        }
     }
     else {
         sum = 0;
     }
 });
 
-console.log(sum);
+console.log(max);
