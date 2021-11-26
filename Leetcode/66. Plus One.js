5086a38028f2b082b63756c0067ec76e063a5adb
Pritom <pritomcsedu.56@gmail.com>
2021-11-26 16:00:07 +0600

    --- Auto-Git Commit ---

--- Leetcode/66. Plus One.js
+++ Leetcode/66. Plus One.js
@@ -1,14 +1,25 @@
-let digits = [1, 2, 3];
+let digits = [9, 9, 9];
 
 let length = digits.length - 1;
 
 let lastDigit = digits[length] + 1;
 
-if (lastDigit !== 0) {
+if (lastDigit % 10 !== 0) {
     digits[length] = lastDigit;
 }
 else {
-    for (i = length - 1; i >= 0; i--) {
-
+    for (i = length; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+            if (i === 0) {
+                digits.unshift(1);
+            }
+        }
+        else {
+            digits[i] += 1;
+            break;
+        }
     }
 }
+
+console.log(digits);
