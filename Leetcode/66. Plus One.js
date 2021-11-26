4213a6189c5287e5a701cea0b8366397a5809d59
Pritom <pritomcsedu.56@gmail.com>
2021-11-26 14:59:54 +0600

    --- Auto-Git Commit ---

--- Leetcode/66. Plus One.js
+++ Leetcode/66. Plus One.js
@@ -1,9 +1,8 @@
 let digits = [1, 2, 3];
 
 for (let i = digits.length - 1, count = 0; i >= 0; i--) {
-    if (digits[i] + 1 % 10 === 0) {
+    if (digits[i] === 9) {
         count = 1;
-        digits[i] = 0;
     }
     else {
         if (count === 1) {
