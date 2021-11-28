5cf214f5950fa437ee1dbd586db10ce4b215f33e
Pritom <pritomcsedu.56@gmail.com>
2021-11-29 00:42:21 +0600

    --- Auto-Git Commit ---

--- Leetcode/88. Merge Sorted Array.js
+++ Leetcode/88. Merge Sorted Array.js
@@ -4,14 +4,22 @@ let i = 0, j = 0, boro = false, choto = false;
 
 nums1.length = m;
 
-while (i < m + j) {
+while (i < m + n) {
     if (nums2[j] < nums1[i] && i == 0) {
         nums1.unshift(nums2[j++]);
     }
-    else if (nums2[j] > nums1[i]) {
-        if (i >= m - 1) {
-
+    else if (nums2[j] >= nums1[i]) {
+        if (i >= nums1.length - 1) {
             nums1.push(nums2[j++]);
+            i++;
+        }
+        else if (nums2[j] < nums1[i + 1]) {
+            nums1.splice(++i, 0, nums2[j++]);
         }
     }
+    else {
+        i++;
+    }
 }
+
+console.log(nums1);
