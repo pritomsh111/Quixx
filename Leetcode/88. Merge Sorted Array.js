a685cfcb21664aafcfd1287a991618a69a9b9c40
Pritom <pritomcsedu.56@gmail.com>
2021-11-28 23:12:03 +0600

    --- Auto-Git Commit ---

--- Leetcode/88. Merge Sorted Array.js
+++ Leetcode/88. Merge Sorted Array.js
@@ -1,5 +1,9 @@
 let nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3;
 
-for (let i = 0, j = 0; i < m + n; i++) {
+let i = 0, j = 0;
 
+while (i < m) {
+    if (nums1[i] < nums2[j]) {
+        nums1.splice(i, 0, nums2[j]);
+    }
 }
