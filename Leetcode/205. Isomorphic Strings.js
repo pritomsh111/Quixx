2f9c736b6672c0d1588812762da1386d27b42304
Pritom <pritomcsedu.56@gmail.com>
2021-11-29 17:46:03 +0600

    --- Auto-Git Commit ---

--- Leetcode/205. Isomorphic Strings.js
+++ Leetcode/205. Isomorphic Strings.js
@@ -1,27 +1,34 @@
 var isIsomorphic = function (s, t) {
-    let map = new Map();
-    let set = new Set();
-
-    let length = s.length, i = 0;
-
-    while (i < length) {
-        if (!map.has(s[i])) {
-            if (!set.has(t[i])) {
-                map.set(s[i], t[i]);
-                set.add(t[i]);
-            }
-            else {
-                return false;
-            }
+    for (let index = 0; index < s.length; ++index) {
+        console.log(s.indexOf(s[index]), t.indexOf(t[index]))
+        if (s.indexOf(s[index]) != t.indexOf(t[index])) {
+            return false;
         }
-        else {
-            if (map.get(s[i]) !== t[i]) {
-                return false;
-            }
-        }
-        i++;
     }
     return true;
+    // let map = new Map();
+    // let set = new Set();
+
+    // let length = s.length, i = 0;
+
+    // while (i < length) {
+    //     if (!map.has(s[i])) {
+    //         if (!set.has(t[i])) {
+    //             map.set(s[i], t[i]);
+    //             set.add(t[i]);
+    //         }
+    //         else {
+    //             return false;
+    //         }
+    //     }
+    //     else {
+    //         if (map.get(s[i]) !== t[i]) {
+    //             return false;
+    //         }
+    //     }
+    //     i++;
+    // }
+    // return true;
 };
 
 
