fa773e8e7197f3b6a6e4063c601c3fd1f167c469
Pritom <pritomcsedu.56@gmail.com>
2021-11-29 13:15:03 +0600

    --- Auto-Git Commit ---

--- Leetcode/202. Happy Number.js
+++ Leetcode/202. Happy Number.js
@@ -1,9 +1,21 @@
-let n = 19, sum, dup;
-while (n > 1) {
-    sum = 0;
-    dup = n;
-    while (dup > 0) {
-        sum += (dup % 10) ** 2;
-        dup /= 10;
+let n = 2;
+var isHappy = function (n) {
+    let sum, dup = n;
+    let set = new Set();
+    while (dup !== 1) {
+        sum = 0;
+        while (dup > 0) {
+            sum += (dup % 10) ** 2;
+            dup = Math.floor(dup / 10);
+        }
+        if (!set.has(sum)) {
+            set.add(sum);
+            dup = sum;
+        }
+        else {
+            return false;
+        }
     }
-}
+    return true;
+};
+console.log(isHappy(n));
