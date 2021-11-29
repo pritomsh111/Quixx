4c669f52830db4b1fb43a7ed3fc85122de23967d
Pritom <pritomcsedu.56@gmail.com>
2021-11-29 21:16:49 +0600

    --- Auto-Git Commit ---

--- Leetcode/680. Valid Palindrome II.js
+++ Leetcode/680. Valid Palindrome II.js
@@ -1,9 +1,28 @@
-let s = "aba";
+var validPalindrome = function (s) {
 
-let l1 = s.length - 1, j = l1, i = 0;
+    let l1 = s.length - 1, j = l1, i = 0, count = 0;
 
-while (i <= l1 / 2) {
-    if (s[i] === s[j]) {
-        i++;
+    while (i <= Math.ceil(l1 / 2)) {
+        if (s[i] === s[j]) {
+            i++;
+            j--;
+        }
+        else if (count++ === 0) {
+            if (s[i] === s[j - 1]) {
+                j--;
+            }
+            else if (s[i + 1] === s[j]) {
+                i++;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
     }
-}
+    return true;
+};
+
+console.log(validPalindrome("abca"));
