cce3666db1cdba09a17e06ebc5da4df1fe63eeaf
Pritom <pritomcsedu.56@gmail.com>
2021-12-01 18:31:56 +0600

    --- Auto-Git Commit ---

--- Leetcode/169. Majority Element.js
+++ Leetcode/169. Majority Element.js
@@ -2,12 +2,12 @@ var majorityElement = function (nums) {
     let map = new Map();
     let length = nums.length;
     for (let i = 0; i < length; i++) {
-        map[nums[i]] ? 0 : map[nums[i]]++;
-        if (map[nums[i]] > Math.ceil(length / 2)) {
+        map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1;
+        if (map[nums[i]] > length / 2) {
             return nums[i];
         }
     }
 };
 
-let nums = [3, 2, 3];
+let nums = [2, 2, 1, 1, 1, 2, 2];
 console.log(majorityElement(nums));
