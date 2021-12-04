06e71b93e83d7d81e69bbc02674bc4c6d1768611
Pritom <pritomcsedu.56@gmail.com>
2021-12-04 19:18:42 +0600

    --- Auto-Git Commit ---

--- Leetcode/1935. Maximum Number of Words You Can Type.js
+++ Leetcode/1935. Maximum Number of Words You Can Type.js
@@ -1,11 +1,13 @@
 var canBeTypedWords = function (text, brokenLetters) {
     let count = 0;
-    text.split(" ").map(item => {
-        if (item.search(/[^brokenLetters]/) >= 0) {
+    let reg = new RegExp(`[${brokenLetters}]`);
+    text = text.split(" ");
+    for (let i = 0; i < text.length; i++) {
+        if (text[i].search(reg) >= 0) {
             count++;
         }
-    });
-    return count;
+    }
+    return text.length - count;
 };
 let text = "hello world", brokenLetters = "ad";
 console.log(canBeTypedWords(text, brokenLetters));
