c254707201fe5fe2c22957bbded4f113ce3fda9c
Pritom <pritomcsedu.56@gmail.com>
2021-12-06 14:28:41 +0600

    --- Auto-Git Commit ---

--- Leetcode/409. Longest Palindrome.js
+++ Leetcode/409. Longest Palindrome.js
@@ -1,22 +1,23 @@
 var longestPalindrome = function (s) {
-    let map = new Map(), l = s.length, sum = 0, max = 0;
+    let l = s.length, onFlag = 0, sum = 0;
+    let map = new Map();
     for (let i = 0; i < l; i++) {
-        map[s[i]] ? map[s[i]]++ : (map[s[i]] = 1, map.set(s[i]));
+        map[s[i]] ? map[s[i]]++ : (map[s[i]] = 0, map.set(s[i]));
     }
     for (let key of map.keys()) {
         if (map[key] % 2 === 0) {
             sum += map[key];
         }
         else {
-            if (map[key] > 1) {
-                sum += map[key] - 1;
+            if (!onFlag) {
+                sum += map[key];
             }
-            else if (map[key] === 1) {
-                max = 1;
+            else {
+                sum += (map[key] - 1);
             }
         }
     }
-    return sum + max;
+    return sum;
 };
 
 console.log(longestPalindrome("abccccdd"));
