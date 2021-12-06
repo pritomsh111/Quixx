b40082f9b11ec93e8484c85fac67065435c72799
Pritom <pritomcsedu.56@gmail.com>
2021-12-06 23:31:32 +0600

    --- Auto-Git Commit ---

--- Leetcode/290. Word Pattern.js
+++ Leetcode/290. Word Pattern.js
@@ -4,7 +4,26 @@
  * @return {boolean}
  */
 var wordPattern = function (pattern, s) {
-
+    let mapP = new Map();
+    let mapS = new Map();
+    let pLen = pattern.length;
+    s = s.split(" ");
+    let sLen = s.length;
+    if (pLen !== sLen) {
+        return false;
+    }
+    else {
+        for (let i = 0; i < pLen; i++) {
+            if (!mapP.has(pattern[i]) && !mapS.has(s[i])) {
+                map.set(pattern[i], s[i]);
+                mapS.set(s[i], pattern[i]);
+            }
+            else if (s[i] !== map.get(pattern[i])) {
+                return false;
+            }
+        }
+    }
+    return true;
 };
 
 let pattern = "abba", s = "dog cat cat fish";
