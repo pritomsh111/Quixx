dad266b82090b9db0219f2be74856ba5bc36399f
Pritom <pritomcsedu.56@gmail.com>
2021-12-06 18:30:06 +0600

    --- Auto-Git Commit ---

--- Leetcode/414. Third Maximum Number.js
+++ Leetcode/414. Third Maximum Number.js
@@ -1,20 +1,19 @@
 var thirdMax = function (nums) {
     let set = new Set(nums);
-    let arr = [], max = -2147483647, mid = -2147483647, min = -2147483647;
+    let arr = [-2147483648];
     for (let i of set.values()) {
-        if (i > max) {
-            max = i;
+        if (i >= arr[0]) {
             arr.unshift(i);
         }
-        else if (i > arr[1]) {
+        else if (i >= arr[1]) {
             arr.splice(1, 0, i);
         }
-        else if (i > arr[2]) {
+        else if (i >= arr[2]) {
             arr.splice(2, 0, i);
         }
     }
-    return arr[2] ? arr[2] : arr[0];
+    return arr.length > 3 ? arr[2] : arr[0];
 };
 
-let nums = [3, 3, 4, 3, 4, 3, 0, 3, 3];
+let nums = [1, 2, -2147483648];
 console.log(thirdMax(nums));
