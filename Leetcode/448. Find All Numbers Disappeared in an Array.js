93233963b82455f9ebfbd9b3eaf0dc9d7f45b4b7
Pritom <pritomcsedu.56@gmail.com>
2021-12-08 22:44:48 +0600

    --- Auto-Git Commit ---

--- Leetcode/448. Find All Numbers Disappeared in an Array.js
+++ Leetcode/448. Find All Numbers Disappeared in an Array.js
@@ -1,15 +1,15 @@
 var findDisappearedNumbers = function (nums) {
-    let l = nums.length, r = [];
-    for (let i = 1; i <= l; i++) {
+    let l = nums.length, r = [], rr = [];
+    for (let i = 0; i < l; i++) {
         r[nums[i]] = true;
     }
     for (let i = 1; i <= l; i++) {
-        if (!r[nums[i]]) {
-            r.push(i);
+        if (!r[i]) {
+            rr.push(i);
         }
     }
-    return r;
+    return rr;
 };
 
-let nums = [4, 3, 2, 7, 8, 2, 3, 1];
-findDisappearedNumbers(nums);
+let nums = [4, 3, 2, 2, 3, 1];
+console.log(findDisappearedNumbers(nums));
