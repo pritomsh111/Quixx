d9a7010e6d4a4b2df5f2b14b5cd909ede03f8906
Pritom <pritomcsedu.56@gmail.com>
2021-12-14 12:41:34 +0600

    --- Auto-Git Commit ---

---
+++ Leetcode/704. Binary Search.js
@@ -0,0 +1,18 @@
+var search = function (nums, target, left = 0, right = nums.length - 1) {
+    while (left <= right) {
+        let mid = left + (right - left) / 2;
+        if (nums[mid] === target) {
+            return mid;
+        }
+        else if (nums[mid] > target) {
+            right = mid - 1;
+        }
+        else if (nums[mid] < target) {
+            left = mid + 1;
+        }
+    }
+    return -1;
+};
+
+let nums = [-1, 0, 3, 5, 9, 12], target = 9;
+console.log(search(nums, target));
