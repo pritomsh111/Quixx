c5d456eca0d9bfcd82dd38b1d718c53e76ca39ee
Pritom <pritomcsedu.56@gmail.com>
2021-12-16 14:35:37 +0600

    --- Auto-Git Commit ---

--- Leetcode/189. Rotate Array.js
+++ Leetcode/189. Rotate Array.js
@@ -1,12 +1,19 @@
 var rotate = function (nums, k) {
-    let temp = null;
-    let l = nums.length;
-    for (let i = 0; i < k % l; i++) {
-        temp = nums.pop();
-        nums.unshift(temp);
+    const reverse = (start, end) => {
+        while (end > start) {
+            let temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            end--;
+            start++;
+        }
     }
+    let l = nums.length;
+    k = k % l;
+    reverse(0, l - 1);
+    reverse(0, k - 1);
+    reverse(k, l - 1);
     return nums;
 };
-
 let nums = [1, 2, 3, 4, 5, 6, 7], k = 3;
 console.log(rotate(nums, k));
