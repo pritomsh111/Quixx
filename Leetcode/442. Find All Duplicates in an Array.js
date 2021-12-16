8778737a8d5cf40405d36b91295f135a17bacb50
Pritom <pritomcsedu.56@gmail.com>
2021-12-16 20:06:49 +0600

    --- Auto-Git Commit ---

--- Leetcode/442. Find All Duplicates in an Array.js
+++ Leetcode/442. Find All Duplicates in an Array.js
@@ -6,7 +6,7 @@ var findDuplicates = function (nums) {
             res.push(nums[i]);
         }
         else {
-            nums[nums[i] - 1] = -nums[i];
+            nums[Math.abs(nums[i]) - 1] = -nums[i];
         }
     }
 
@@ -14,4 +14,4 @@ var findDuplicates = function (nums) {
 };
 
 let nums = [4, 3, 2, 7, 8, 2, 3, 1];
-findDuplicates(nums);
+console.log(findDuplicates(nums));
