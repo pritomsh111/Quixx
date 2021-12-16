24bad36d9428beff6b99c52d8e1095f46cb44b88
Pritom <pritomcsedu.56@gmail.com>
2021-12-16 16:05:56 +0600

    --- Auto-Git Commit ---

--- Leetcode/283. Move Zeroes.js
+++ Leetcode/283. Move Zeroes.js
@@ -1,14 +1,26 @@
 var moveZeroes = function (nums) {
-    let l = nums.length, j = 0;
-    for (let i = 0; i < l; i++) {
-        if (!nums[j]) {
-            nums.splice(j, 1);
-            nums.push(0);
+    // let l = nums.length, j = 0;
+    // for (let i = 0; i < l; i++) {
+    //     if (!nums[j]) {
+    //         nums.splice(j, 1);
+    //         nums.push(0);
+    //     }
+    //     else {
+    //         j++;
+    //     }
+    // }
+    let l = nums.length;
+    let zero = 0, i = 0;
+    while (i < l) {
+        if (nums[i]) {
+            nums[zero++] = nums[i];
         }
-        else {
-            j++;
-        }
+        i++;
     }
+    while (zero < l) {
+        nums[zero++] = 0;
+    }
+    return nums;
 };
-let nums = [0, 0, 1];
-moveZeroes(nums);
+let nums = [0, 1, 0, 3, 12];
+console.log(moveZeroes(nums));
