b5a02f18d78c25bfde094ebba67ce5a527f820f0
Pritom <pritomcsedu.56@gmail.com>
2021-12-22 19:43:41 +0600

    --- Auto-Git Commit ---

--- Leetcode/567. Permutation in String.js
+++ Leetcode/567. Permutation in String.js
@@ -1,31 +1,38 @@
 var checkInclusion = function (s1, s2) {
     let s1L = s1.length;
     let s2L = s2.length;
-    let map2 = {};
-    let count = 0, j = 0;
     if (s2L < s1L) {
         return false;
     }
-    let map = {};
+    let map = {}, map2 = {};
+
+    const check = () => {
+        for (let [key, val] of Object.entries(map)) {
+            if (map2[key] !== val) {
+                return false;
+            }
+        }
+        return true;
+    }
     for (let i = 0; i < s1L; i++) {
         map[s1[i]] ? map[s1[i]]++ : map[s1[i]] = 1;
     }
-    for (let i = 0; i < s2L; i++) {
-        if (map[s2[i]]) {
-            map2 = {}, count = 0, j = s1L;
-            while (j-- > 0) {
-                map2[s2[i]] ? map2[s2[i]]++ : map2[s2[i]] = 1;
-                if (map2[s2[i]] === map[s1[i]]) {
-                    count++;
-                }
-                else if (map2[s2[i]] > map[s1[i]]) {
-                    break;
-                }
+    for (let i = 0, j = 0; i < s2L; i++) {
+        if (i < s1L) {
+            map2[s2[i]] ? map2[s2[i]]++ : map2[s2[i]] = 1;
+        }
+        else {
+            if (check()) {
+                return true;
+            }
+            else {
+                map2[s2[i]]++;
+                map2[s2[j++]]--;
             }
         }
     }
-    return count === s1L;
+    return false;
 };
 
-let s1 = "ab", s2 = "eidbaooo";
+let s1 = "ab", s2 = "aiiiiiibass";
 console.log(checkInclusion(s1, s2));
