7bb2affbaad5cb88c583eae45c4118cad184eee0
Pritom <pritomcsedu.56@gmail.com>
2022-05-11 18:00:35 +0600

    --- Auto-Git Commit ---

---
+++ Node/file.js
@@ -0,0 +1,17 @@
+const file = require('fs');
+const path = require('path');
+
+file.readFile(`${path.join(__dirname, 'demo', 'text.txt')}`, 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(data);
+})
+
+file.writeFile(`${__dirname + '\\' + 'file.txt'}`, 'utf8', (err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+});
