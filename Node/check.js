c2a6a2a6bdbef8f6c178b3a9ad8c249916b741da
Pritom <pritomcsedu.56@gmail.com>
2022-05-16 20:57:00 +0600

    --- Auto-Git Commit ---

---
+++ Node/check.js
@@ -0,0 +1,15 @@
+document.querySelector('.a').addEventListener('click', (e) => {
+    console.log("1");
+});
+document.querySelector('.b').addEventListener('click', (e) => {
+    console.log("2");
+});
+document.querySelector('.c').addEventListener('click', (e) => {
+    console.log("3");
+});
+document.querySelector('.d').addEventListener('click', (e) => {
+    console.log("4");
+});
+document.querySelector('.e').addEventListener('click', (e) => {
+    console.log("5");
+});
