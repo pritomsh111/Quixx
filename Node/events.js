1ad9ccbbcf435c265026ede9c35b721c5e1ae350
Pritom <pritomcsedu.56@gmail.com>
2022-05-18 16:38:44 +0600

    --- Auto-Git Commit ---

--- Node/events.js
+++ Node/events.js
@@ -1,6 +1,8 @@
 const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
-eventEmitter.emit('my_event');
+
 eventEmitter.on('my_event', () => {
     console.log('data received successfully.');
 });
+
+eventEmitter.emit('my_event');
