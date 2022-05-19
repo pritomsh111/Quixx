d097212378dd8cbe2910bc3ec92f7b0a7925d14f
Pritom <pritomcsedu.56@gmail.com>
2022-05-19 14:15:30 +0600

    --- Auto-Git Commit ---

--- Node/http.js
+++ Node/http.js
@@ -1,4 +1,14 @@
 const http = require('http');
+const fs = require('fs');
 
-console.log(http.STATUS_CODES);
-console.log(http.METHODS);
+// console.log(http.STATUS_CODES);
+// console.log(http.METHODS);
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+    let txt = fs.readFileSync(`${__dirname}\\file.txt`, 'utf8');
+    res.end(txt);
+});
+
+server.listen(5000);
