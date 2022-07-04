1d275caefccb9e5fc7505a4a396ee84da88fd83e
Pritom <pritomcsedu.56@gmail.com>
2022-07-04 19:04:10 +0600

    --- Auto Git Commit ---

--- starter-01/starter/db/connect.js
+++ starter-01/starter/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://tasks-01:<password>@app-1.nlziv.mongodb.net/?retryWrites=true&w=majority';
+const connectionString = 'mongodb+srv://tasks-01:1234@app-1.nlziv.mongodb.net/?retryWrites=true&w=majority';
 
 const connectDB = (url) => {
     return mongoose.connect(connectionString);
