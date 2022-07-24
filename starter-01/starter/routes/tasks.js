06e3971dc575d6d26603811aded0734540744064
Pritom <pritomcsedu.56@gmail.com>
2022-07-24 14:22:14 +0600

    --- Auto Git Commit ---

--- starter-01/starter/routes/tasks.js
+++ starter-01/starter/routes/tasks.js
@@ -4,6 +4,6 @@ const routes = express.Router();
 const { getAllTasks, createTask, updateTask, deleteTask, getSingleTask } = require('../controllers/tasks');
 
 routes.route('/').get(getAllTasks).post(createTask);
-routes.route('/:id').get(getSingleTask).put(updateTask).delete(deleteTask);
+routes.route('/:id').get(getSingleTask).patch(updateTask).delete(deleteTask);
 
 module.exports = routes;
