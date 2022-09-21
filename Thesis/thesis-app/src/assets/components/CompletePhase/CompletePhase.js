1ee4f7dceff9be63b37a2803623b2229b2e0afcd
Pritom <pritomcsedu.56@gmail.com>
2022-09-21 21:16:36 +0600

    --- Auto Git Commit ---

--- Thesis/thesis-app/src/assets/components/CompletePhase/CompletePhase.js
+++ Thesis/thesis-app/src/assets/components/CompletePhase/CompletePhase.js
@@ -1,15 +1,25 @@
 import { useState, useEffect } from 'react';
 import classes from './CompletePhase.module.css';
 
-const CompletePhase = ({ state, store }) => {
-
-    return store.map((item, index) => {
-        return (
+const CompletePhase = ({ result, pushResult, store }) => {
+    const [sentence, setSentence] = useState('');
+    return (
+        <>
             <div className={classes.CompletePhase}>
-                <img className={classes.big} alt={item} key={`${item + index}`} src={item} />
+                {
+                    store.map((item, index) =>
+                        <div key={`${item + index}`} className={`${classes.image} ${(result[pushResult, index].charCodeAt(0) >= 65 && result[pushResult, index].charCodeAt(0) <= 90) ? classes.big : ''}`}>
+                            <img alt={item} src={item} />
+                        </div>
+                    )
+                }
             </div>
-        )
-    });
+            <div className={classes.Sentence}>
+                <input type="text" value={sentence} onChange={({ target }) => setSentence(target.value)} />
+                <button onClick={() => { pushResult([...result, sentence]); }}>Next</button>
+            </div>
+        </>
+    );
 }
 
 export default CompletePhase;
