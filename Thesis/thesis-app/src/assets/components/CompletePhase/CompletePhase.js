c7d91ea8c16ffd1758c01a41d254c395b98d95fb
Pritom <pritomcsedu.56@gmail.com>
2022-09-21 19:45:35 +0600

    --- Auto Git Commit ---

--- Thesis/thesis-app/src/assets/components/CompletePhase/CompletePhase.js
+++ Thesis/thesis-app/src/assets/components/CompletePhase/CompletePhase.js
@@ -1,8 +1,15 @@
 import { useState, useEffect } from 'react';
+import classes from './CompletePhase.module.css';
 
-const CompletePhase = (props) => {
-    console.log(props);
-    return ("");
+const CompletePhase = ({ state, store }) => {
+
+    return store.map((item, index) => {
+        return (
+            <div className={classes.CompletePhase}>
+                <img className={classes.big} alt={item} key={`${item + index}`} src={item} />
+            </div>
+        )
+    });
 }
 
 export default CompletePhase;
