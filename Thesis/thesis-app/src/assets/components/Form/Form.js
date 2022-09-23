ed23b22ec1923150f48629af332ffa925bdb5436
Pritom <pritomcsedu.56@gmail.com>
2022-09-23 19:32:19 +0600

    --- Auto Git Commit ---

--- Thesis/thesis-app/src/assets/components/Form/Form.js
+++ Thesis/thesis-app/src/assets/components/Form/Form.js
@@ -10,6 +10,7 @@ const Form = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [slider, setSlider] = useState(8);
+    const [userExist, setUserExist] = useState("");
     const history = useNavigate();
 
     const login = async () => {
@@ -25,15 +26,31 @@ const Form = () => {
         return user;
     }
 
+    const redirectAfterLogin = async () => {
+        const user = await login();
+        if (user) {
+            const { data: { images, password, trainingTime } } = user;
+            history('/login', { state: { images, password, trainingTime } });
+        }
+        else {
+            setUserExist("User Not Found!");
+        }
+    }
+
     return (
         <div className={classes.Form}>
             <div className={classes.FormChild}>
                 <h1>Registration Form</h1>
                 <div className={classes.Inputs}>
-                    <input type="text" placeholder='Name'
-                        value={name} onChange={({ target }) => { setName(target.value) }} />
-                    <input type="email" placeholder='Email'
-                        value={email} onChange={({ target }) => { setEmail(target.value) }} />
+                    <div className={classes.nameInputs}>
+                        <input type="text" placeholder='Name'
+                            value={name} onChange={({ target }) => { setName(target.value); setUserExist(""); }} />
+                        <span>{userExist}</span>
+                    </div>
+                    <div className={classes.emailInputs}>
+                        <input type="email" placeholder='Email'
+                            value={email} onChange={({ target }) => { setEmail(target.value); setUserExist(""); }} />
+                    </div>
                     <div className={classes.slider}>
                         <label htmlFor='slider'>Password Size: {slider}</label>
                         <input id="slider" type='range' value={slider} onChange={({ target }) => { setSlider(target.value) }} min={4} max={14} />
@@ -42,13 +59,11 @@ const Form = () => {
 
                 <div className={classes.submit}>
                     <button type="submit" onClick={async () => {
-                        let exists = await login();
-                        console.log(exists + "dsad");
-                        !exists ? await axios.post(`${process.env.REACT_APP_URL}/registration`, { name, email }) : console.log("ache");
-
-                        // return name && email ? history('/passwordShow', {state: randPassGenerator(slider) }) : '';
+                        let { data } = await login();
+                        !data ? await axios.post(`${process.env.REACT_APP_URL}/registration`, { name, email }) : setUserExist("User Exists! Try Login");
+                        return name && email ? history('/passwordShow', { state: randPassGenerator(slider) }) : '';
                     }}>Registration</button>
-                    <button type="submit" onClick={login}>Login</button>
+                    <button type="submit" onClick={redirectAfterLogin}>Login</button>
                 </div>
             </div>
         </div >
