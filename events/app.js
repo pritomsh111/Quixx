003ff181f925f47fce84a666d5b9c91dee0deee4
Pritom <pritomcsedu.56@gmail.com>
2022-10-12 22:28:56 +0600

    --- Auto Git Commit ---

---
+++ events/app.js
@@ -0,0 +1,19 @@
+document.querySelector('.a').addEventListener('click', function (e) {
+    console.log('a');
+}, false);
+
+document.querySelector('.b').addEventListener('click', function (e) {
+    console.log('b');
+}, false);
+
+document.querySelector('.c').addEventListener('click', function (e) {
+    console.log('c');
+}, false);
+
+document.querySelector('.d').addEventListener('click', function (e) {
+    console.log('d');
+}, false);
+
+document.querySelector('.e').addEventListener('click', function (e) {
+    console.log('e');
+}, false);
