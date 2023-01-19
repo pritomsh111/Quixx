3a04341824e311b5173f1942bc9e3037d0c12bc7
Pritom <pritomcsedu.56@gmail.com>
2023-01-19 23:30:14 +0600

    --- Auto Git Commit ---

--- Thesis/thesis-app/src/assets/utility/randomPassGenerator.js
+++ Thesis/thesis-app/src/assets/utility/randomPassGenerator.js
@@ -1,11 +1,48 @@
+const generateUppercase = () => {
+    return process.env.REACT_APP_CHARACTERS_UPPERCASE.charAt(Math.random() * process.env.REACT_APP_CHARACTERS_UPPERCASE.length);
+}
+const generateLowercase = () => {
+    return process.env.REACT_APP_CHARACTERS_LOWERCASE.charAt(Math.random() * process.env.REACT_APP_CHARACTERS_LOWERCASE.length);
+}
+const generateNumber = () => {
+    return process.env.REACT_APP_CHARACTERS_NUMBER.charAt(Math.random() * process.env.REACT_APP_CHARACTERS_NUMBER.length);
+}
+const generateSpecial = () => {
+    return process.env.REACT_APP_SPECIAL_CHARACTERS.charAt(Math.random() * process.env.REACT_APP_SPECIAL_CHARACTERS.length);
+}
 export const randPassGenerator = (length) => {
-    let password = [], specialCount = 0;
-    length < 10 ? specialCount = 2 : specialCount = 3;
-    while (length--) {
-        password.push(process.env.REACT_APP_CHARACTERS.charAt(Math.floor(Math.random() * process.env.REACT_APP_CHARACTERS.length)));
+    let password = [], count = 0;
+    while (password.length <= 12) {
+        password.push(generateUppercase());
     }
-    while (specialCount--) {
-        password[Math.floor(Math.random() * password.length)] = process.env.REACT_APP_SPECIAL_CHARACTERS.charAt(Math.floor(Math.random() * process.env.REACT_APP_SPECIAL_CHARACTERS.length));
+    while (count++ < 3) {
+        let dummy = Math.floor(Math.random() * 12);
+        if (/[A-Z]/.test(password[dummy])) {
+            password[dummy] = generateSpecial();
+        }
+        else {
+            count--;
+        }
+    }
+    count = Math.floor(Math.random() * 4) + 2;
+    while (count-- >= 0) {
+        let dummy = Math.floor(Math.random() * 12);
+        if (/[A-Z]/.test(password[dummy])) {
+            password[dummy] = generateLowercase();
+        }
+        else {
+            count++;
+        }
+    }
+    count = Math.floor(Math.random() * 2) + 1;
+    while (count-- >= 0) {
+        let dummy = Math.floor(Math.random() * 12);
+        if (/[A-Z]/.test(password[dummy])) {
+            password[dummy] = generateNumber();
+        }
+        else {
+            count++;
+        }
     }
     return password;
 }
